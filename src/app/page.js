c378bbdac0b0552dc67ff39e976f9f9cc49643d1
Nurunnisa Fathanah <dzsbnf21d@student.unhas.ac.id>
2023-11-09 15:42:53 +0800

    feat: Menambahkan fitur ubah nama dan bio

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,17 @@
+'use client'
+
+import { useState } from 'react'
 import Image from 'next/image'
 import './globals.css'
 
 export default function Home() {
+
+  const [hai, setHai] = useState(0);
+  const handlerHaiAdd = () => setHai(hai+1);
+
+  const [nama, setNama] = useState('Nurunnisa Fathanah');
+  const handlerGantiNama = () => setNama('Fathanah');
+
   return (
     <div className='body'>
       <div className="banner-container">
@@ -19,18 +29,21 @@ export default function Home() {
           </div>
           <div className="content-header-banner">
             {/* nama n info */}
-            <h1>Nurunnisa Fathanah</h1>
+            <h1>{nama}</h1>
             <div className="bio-nim-header-banner">
               {/* nim n bio */}
               <p>D121211002</p>
-              <p>IF'21 UH</p>
+              <p>IF'21 UH - Hai ke-{hai}</p>
             </div>
           </div>
         </div>
         <div className="cta-banner-wrapper">
         {/* cta */}
-        <button>
+        <button onClick={handlerHaiAdd}>
           <p>Halo!</p>
+        </button>
+        <button style={{marginTop:'24px'}}onClick={handlerGantiNama}>
+          <p>Ganti Nama</p>
         </button>
         </div>
       </div>
