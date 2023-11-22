b1b819d2d641b845fb6e00f9bc96d1ab053ac294
Nurunnisa Fathanah <dzsbnf21d@student.unhas.ac.id>
2023-11-22 22:47:32 +0800

    refactor: text when API is loading

--- src/app/diary/page.js
+++ src/app/diary/page.js
@@ -51,7 +51,7 @@ export default function Diary() {
                 </ul>
             ):
             (
-                "API not loading"
+                "API is loading"
             )}      
         </>
     )
